f1b13571275411b3124b7dbe3cb9a7ea761e952e
Ahmed Aziz Khelifi <ahmedazizkhelifi@gmail.com>
2018-07-01 23:36:46 +0100

    Ameliorer l'affichage

--- ZCasinoPYT2.py
+++ ZCasinoPYT2.py
@@ -2,6 +2,7 @@
 #ZCasino
 from random import randrange
 from math import ceil
+import os
 
 def isOdd(a,b):
     if a % 2 == 0 :
@@ -52,7 +53,22 @@ def again():
     
 
 #PP
-Argent = 1000
+print("""La roulette est constituée de 50 cases allant de 0 à 49. Les numéros pairs sont de couleur noire, les numéros impairs sont de couleur rouge.
+Le joueur mise sur un numéro compris entre 0 et 49. En choisissant son numéro, il y dépose la somme qu'il souhaite miser.
+Le croupier lance la roulette, lâche la bille et quand la roulette s'arrête, relève le numéro de la case dans laquelle la bille s'est arrêtée. Le numéro sur lequel s'est arrêtée la bille est, naturellement, le numéro gagnant.
+Si le numéro gagnant est celui sur lequel le joueur a misé, le croupier lui remet 3 fois la somme misée.
+Sinon, le croupier regarde si le numéro misé par le joueur est de la même couleur que le numéro gagnant. Si c'est le cas, le croupier lui remet 50 % de la somme misée. Si ce n'est pas le cas, le joueur perd sa mise.""")
+print ''
+
+while True:
+    Argent_ch = raw_input("Argent initial en $:")
+    try:
+        Argent=int(Argent_ch)
+    except:
+        continue
+    if Argent > 0 :
+        break
+Arginit = Argent
 number()
 while True :
     w = randrange(50)
@@ -65,11 +81,11 @@ while True :
         print '*****Money : {}$*****'.format(Argent)
     elif isOdd(w,n):
          m = ceil(m/2)
-         print 'Vous avez perdu la moitié de votre mise ' + str(m)
+         print 'Vous avez perdu la moitié de votre mise ' + str(m) + '$'
          Argent = Argent - m
          print '*****Money : {}$*****'.format(Argent)
     else :
-        print 'Desole vous avez perdu {}'.format(m)
+        print 'Desole vous avez perdu {}$'.format(m)
         Argent = Argent - m
         print '*****Money : {}$*****'.format(Argent)
     
@@ -78,28 +94,17 @@ while True :
         break
     test = again()
     if test :
-        print 'YES'
         continue
     else:
-        print 'NO'
         break
 print 'Thank you for playing ZCasino'
-if Argent > 1000 :
-    w = Argent - 1000
+if Argent > Arginit :
+    w = Argent - Arginit
     print 'Vous avez gagnez en tatal {}$'.format(w)
-elif Argent < 1000 :
-    w = 1000 - Argent
+elif Argent < Arginit :
+    w = Arginit - Argent
     print 'Vous avez perdu en tatal {}$'.format(w)
 else:
     print "Tu n'as rien perdu n'i gagner "
     
-    
-
-
-    
-    
-
-
-    
-
-    
+os.system("pause") 
